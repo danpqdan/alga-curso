13245da3a2b9cd22757853875f669d850a009ed6
Daniel Santos <danieltisantos@gmail.com>
2022-10-31 16:44:49 -0300

    Iniciado projeto AlgaWorks

---
+++ src/main/java/com/cursoalga/cursoapialga/CursoApiAlgaApplication.java
@@ -0,0 +1,13 @@
+package com.cursoalga.cursoapialga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiAlgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApiAlgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoalga/cursoapialga/CursoApiAlgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoalga.cursoapialga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiAlgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
