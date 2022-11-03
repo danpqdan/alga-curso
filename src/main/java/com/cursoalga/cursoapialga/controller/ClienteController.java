d68ec3871d9d0237173e7d77e49af88994b6776b
Daniel Santos <danieltisantos@gmail.com>
2022-11-03 17:24:53 -0300

    adicionado FlwSQL e Iniciado Entidade JPA

--- src/main/java/com/cursoalga/cursoapialga/controller/ClienteController.java
+++ src/main/java/com/cursoalga/cursoapialga/controller/ClienteController.java
@@ -1,9 +1,10 @@
 package com.cursoalga.cursoapialga.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +12,13 @@ import com.cursoalga.cursoapialga.model.Cliente;
 
 @RestController
 public class ClienteController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Daniel");
-        cliente1.setEmail("@Daniel123");
-        cliente1.setTelefone("123456789");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("maria");
-        cliente2.setEmail("@maria123");
-        cliente2.setTelefone("89898989");
-
-        return Arrays.asList(cliente1, cliente2);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/cursoalga/cursoapialga/model/Cliente.java
+++ src/main/java/com/cursoalga/cursoapialga/model/Cliente.java
@@ -1,12 +1,24 @@
 package com.cursoalga.cursoapialga.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String nome;
     String email;
