3f6f7fcadca747ed6d95ff2985418572f834dc4d
Daniel Santos <danieltisantos@gmail.com>
2022-11-03 17:40:46 -0300

    implementado query personalizada à interface cliente

--- src/main/java/com/cursoalga/cursoapialga/controller/ClienteController.java
+++ src/main/java/com/cursoalga/cursoapialga/controller/ClienteController.java
@@ -2,23 +2,24 @@ package com.cursoalga.cursoapialga.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoalga.cursoapialga.model.Cliente;
+import com.cursoalga.cursoapialga.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return clienteRepository.findAll();
     }
 }
