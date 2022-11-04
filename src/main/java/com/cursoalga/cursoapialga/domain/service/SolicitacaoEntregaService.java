8b409ffcc3bf6193b64df0217ba8d2a2544aedcd
Daniel Santos <danieltisantos@gmail.com>
2022-11-04 18:07:15 -0300

    implementado corpo para o consumidor da api

---
+++ src/main/java/com/cursoalga/cursoapialga/domain/service/SolicitacaoEntregaService.java
@@ -0,0 +1,34 @@
+package com.cursoalga.cursoapialga.domain.service;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.cursoalga.cursoapialga.model.Cliente;
+import com.cursoalga.cursoapialga.model.Entrega;
+import com.cursoalga.cursoapialga.model.StatusEntrega;
+import com.cursoalga.cursoapialga.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class SolicitacaoEntregaService {
+
+    private CatalogoClienteService catalogoClienteService;
+    private EntregaRepository entregaRepository;
+
+    @Transactional
+    public Entrega solicitar(Entrega entrega) {
+        Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+
+        entrega.setCliente(cliente);
+        entrega.setStatus(StatusEntrega.PENDENTE);
+        entrega.setDataPedido(LocalDateTime.now());
+
+        return entregaRepository.save(entrega);
+    }
+
+}
